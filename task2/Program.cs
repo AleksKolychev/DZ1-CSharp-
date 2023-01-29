0e5948e1c44b74ee64ab10425583188616e73ff9
AleksKolychev <aleksushome@gmail.com>
2023-01-29 15:00:31 +0500

    Add task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ое число: ");
+int k = Convert.ToInt32(Console.ReadLine());
+if (n > m && n > k)
+    Console.WriteLine(n);
+else if (m > n && m > k)
+    Console.WriteLine(m);
+else if (k > n && k > m)
+    Console.WriteLine(k);
+else
+    Console.WriteLine("Числа равны");
